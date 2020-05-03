984d524ddc4b57b44bd2c6c6b93adf9d95e10843
hedgehog_noodl <kisaichi07@gmail.com>
2020-05-03 19:24:55 +0900

    今度こそ

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('メッセージを変更してみた');
+const axios = require('axios');
+const qs = require('querystring');
+
+console.log(qs.stringify({
+    message: 'ProtoOut Studioからの通知だよー！GitHub Actionsだよー！',
+    imageThumbnail: "http://randomfox.ca/images/60.jpg",
+    imageFullsize: "http://randomfox.ca/images/60.jpg",
+}))
