9c994d3bea8ce96d87770bbe4917d71e5df0ae55
hedgehog_noodl <kisaichi07@gmail.com>
2020-05-04 15:52:12 +0900

    東上線に戻す

--- yamanote.js
+++ yamanote.js
@@ -13,7 +13,7 @@ async function updateData(newData){
 async function getRequest() {
   let response;
   try {
-    response = await axios.get('https://transit.yahoo.co.jp/traininfo/detail/120/0/');
+    response = await axios.get('https://transit.yahoo.co.jp/traininfo/detail/82/0/');
     let html = response.data;
     html = html.replace(/\r?\n/g,""); //整形1: 改行などを削除して整形しやすくする
     let ttl = html.match(/<h1 class="title">(.*?)<\/h1>/)[0];
